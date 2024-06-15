b53399a8532879c6cb3541492ab85d23d0d96b6b
gustta11 <gustavokeven50@gmail.com>
2024-06-15 16:19:32 -0300

    Configurando criação de tabelas e acessoria

--- backend/model/acessoriaModel.js
+++ backend/model/acessoriaModel.js
@@ -5,7 +5,7 @@ export function read(callback){
     connection.query('SELECT * from acessoria where activo_acessoria = 1', callback)
 }
 export function create(nome_acessoria,contato_acessoria,finalidade_acessoria,cidade_acessoria,estado_acessoria,valor_acessoria,callback){
-    connection.query('INSERT INTO acessoria (nome_acessoria,contato_acessoria,finalidade_acessoria,cidade_acessoria,estado_acessoria,valor_acessoria,activo_grupo) VALUES(?,?,?,?,?,?,1)', [nome_acessoria,contato_acessoria,finalidade_acessoria,cidade_acessoria,estado_acessoria,valor_acessoria], callback)
+    connection.query('INSERT INTO acessoria (nome_acessoria,contato_acessoria,finalidade_acessoria,cidade_acessoria,estado_acessoria,valor_acessoria,activo_acessoria) VALUES(?,?,?,?,?,?,1)', [nome_acessoria,contato_acessoria,finalidade_acessoria,cidade_acessoria,estado_acessoria,valor_acessoria], callback)
 }
 export function update(nome_acessoria,contato_acessoria,finalidade_acessoria,cidade_acessoria,estado_acessoria,valor_acessoria, callback){
     connection.query('UPDATE grupo SET nome_acessoria = ?, contato_acessoria = ?, finalidade_acessoria = ?, cidade_acessoria = ?, estado_acessoria = ?, valor_acessoria = ?, activo_acessoria = 1 WHERE id = ?', [nome_acessoria,contato_acessoria,finalidade_acessoria,cidade_acessoria,estado_acessoria,valor_acessoria], callback)
